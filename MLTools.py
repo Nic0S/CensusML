3fbdc65d16aff411b3dcfc313b580a6f03c3a71f
Nico Schlumprecht <snico1995@gmail.com>
2017-01-30 21:22:24 -0600

    progress on categorizing / one-hot

--- MLTools.py
+++ MLTools.py
@@ -1,10 +1,10 @@
-
+import numpy as np
 
 class InputData:
-    numColumns = -1
-    numRows = 0
-    origData = None
-    modifiedData = []
+    num_columns = -1
+    num_rows = 0
+    original_data = []
+    modified_data = []
 
     skip_columns = []
     categorize_columns = []
@@ -13,29 +13,65 @@ class InputData:
 
     def __init__(self, data, delimeter=",", skip_columns=[], categorize_columns=[]):
 
-        # Copy this to avoid the user changing data?
-        self.origData = data
-
         self.skip_columns = skip_columns
         self.categorize_columns = categorize_columns
 
         for row in data:
-            split = row.strip().split(delimeter)
+            split_row = row.strip().split(delimeter)
+            self.original_data.append(split_row)
 
-            # First row, determine column info
-            if self.numColumns == -1:
-                self.init_columns(len(split))
+        self.original_data = np.array(self.original_data)
 
-    def init_columns(self, num_columns):
-        self.numColumns = num_columns
+        self.num_columns = self.original_data.shape[1]
 
-        for i in range(0, num_columns):
+        for i in range(0, self.num_columns):
+            if i in self.skip_columns:
+                continue
+
             if i in self.categorize_columns:
+                column = self.original_data[:, i]
                 self.column_categories.append([])
+
+                for unique_item in set(column):
+                    self.column_categories[-1].append(unique_item)
+
             else:
                 self.column_categories.append(None)
 
         print(self.column_categories)
+
+
+
+
+        # for row in data:
+        #     split_row = row.strip().split(delimeter)
+        #
+        #     for i, cell in enumerate(split_row):
+        #
+        #         # This row needs to be categorized
+        #         if i in categorize_columns:
+        #
+        #             # This category has already been made
+        #             if cell in self.column_categories[i]:
+        #
+        #                 # Get the category value from column_categories and add it to the row
+        #                 modified_row.append(self.column_categories[i].index(cell))
+        #
+        #             # Category has not been seen yet, create it
+        #             else:
+        #                 self.column_categories[i].append(cell)
+        #                 modified_row.append(len(self.column_categories[i]) - 1)
+        #
+        #         else:
+        #             modified_row.append(cell)
+        #
+        #     print(modified_row)
+
+
+
+
+
+
 
 
 
